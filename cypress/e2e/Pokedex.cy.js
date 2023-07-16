77c07331f16af7b15a5b7bb9e3f677fec554d1dd
Dali Guerfali <daliguerfali@gmail.com>
2023-07-16 18:45:01 +0100

    pokemon page test

--- cypress/e2e/Pokedex.cy.js
+++ cypress/e2e/Pokedex.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('charizard').click()
+    cy.contains('solar power')
+  })
+
 })
